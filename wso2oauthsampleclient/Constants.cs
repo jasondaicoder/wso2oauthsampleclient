16e937876d2ee435006f1574d3d94f7ceecba4b9
jason.dai <jason.dai@didata.com.au>
2016-11-30 15:47:54 +1100

    changed to store access token in cookie

---
+++ wso2oauthsampleclient/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wso2oauthsampleclient
+{
+	public static class Constants
+	{
+		public const string AccessToken = "wso2_oauth_accesstoken";
+	}
+}
